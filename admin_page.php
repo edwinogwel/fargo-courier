94219c273b677ad8ea9fad0f49d5b58daf66f3fd
edwinogwel <edogwel@gmail.com>
2024-03-15 16:54:41 +0300

    Implement admin home page

--- admin_page.php
+++ admin_page.php
@@ -1,52 +1,172 @@
 <?php
-   session_start();
+  session_start();
+  require_once('database.php');
+  $users_query = "SELECT * FROM users";
+  $shipments_query = "SELECT * FROM shipments";
+  $users_result = mysqli_query($conn, $users_query);
+  $shipments_result = mysqli_query($conn, $shipments_query);
 ?>
 <?php
-   if(isset($_POST["logout"]))  {
-      session_destroy();
-      header("Location: admin_login.php");
-   }
+  if(isset($_POST["logout"]))  {
+    session_destroy();
+    header("Location: admin_login.php");
+  }
 ?>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Administrator</title>
-   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   <style>
-      body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
-      body {font-size:16px;}
-      .w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
-      .w3-half img:hover{opacity:1}
-   </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Administrator</title>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <style>
+    body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
+    body {font-size:16px;}
+    .w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
+    .w3-half img:hover{opacity:1}
+  </style>
 </head>
 <body>
-   <!-- Navbar -->
-   <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
-      <div class="w3-container">
-         <h3 class="w3-padding-64"><b>Admin<br>Portal</b></h3>
-      </div>
-      <div class="w3-bar-block">
-         <a href="#" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a>
-         <a href="#query_customers" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Customers Report</a>
-      </div>
-   </nav>
 
-   <!-- PAGE CONTENT-->
-   <div class="w3-main" style="margin-left:340px;margin-right:40px">
+  <!-- Navbar -->
+  <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:160px;font-weight:bold;" id="mySidebar"><br>
+    <div class="w3-container">
+      <h3 class="w3-padding-64"><b>Admin<br>Portal</b></h3>
+    </div>
+    <div class="w3-bar-block">
+      <a href="#" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a>
+      <a href="#customers" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Reports</a>
+      <a href="#shipments" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Shipments</a>
+    </div>
+  </nav>
+
+  <!-- PAGE CONTENT-->
+  <div class="w3-main" style="margin-left:170px; margin-right:40px">
+    <!-- Home -->
+    <div class="w3-container" id="send_parcel" style="margin-top:75px">
       <!-- Logout -->
       <form action="admin_page.php" method="post">
-         <br>
-         <input type="submit" name="logout" value="logout" style="margin-left: 90%;">
+        <br>
+        <input type="submit" name="logout" value="logout" style="margin-left: 90%;">
       </form>
 
-      <img src="./images/admin_home.jpg" alt="img">
+      <img src="./images/admin_home.jpg" alt="img" style="height:450px; width:auto">
+    </div><br><br>
 
-   </div>
+    <!-- Customers-->
+    <div class="w3-container" id="customers" style="margin-top:75px margin-left:170px">
+    <h1 class="w3-xxxlarge w3-text-red"><b>Customers.</b></h1>
+      <div class="bg-dark">
+        <div class="container">
+            <div class="row mt-5">
+              <div class="col">
+                  <div class="card mt-5 ">
+                    <div class="card-header">
+                      <h2 class="display-6 text-center">Customers Details</h2>
+                    </div>
+                    <div class="card-body">
+                      <table class="table table-bordered text-center">
+                        <tr class="bg-dark text-white">
+                          <td>ID</td>
+                          <td>Name</td>
+                          <td>Email</td>
+                          <td>Mobile</td>
+                          <td>Address</td>
+                          <td>Username</td>
+                          <td>Reg Date</td>
+                          <td>Delete</td>
+                        </tr>
+                        <tr>
+                        <?php
+                          while($row = mysqli_fetch_assoc($users_result))
+                          {
+                        ?>
+                          <td><?php echo $row['ID']?></td>
+                          <td><?php echo $row['Name']?></td>
+                          <td><?php echo $row['Email']?></td>
+                          <td><?php echo $row['Mobile']?></td>
+                          <td><?php echo $row['Address']?></td>
+                          <td><?php echo $row['Username']?></td>
+                          <td><?php echo $row['Reg Date']?></td>
+                          <td><a href="#" class="btn btn-danger">Delete</a></td>
+                        </tr>
+                          <?php
+                            }
+                          ?>
+                      </table>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        </div><br><br>
+      </div>
+    </div><br>
+
+    <!-- Shipments -->
+    <div class="w3-container" id="shipments" style="margin-top:75px">
+      <h1 class="w3-xxxlarge w3-text-red"><b>Shipments.</b></h1>
+      <div class="bg-dark">
+        <div class="container">
+            <div class="row mt-5">
+              <div class="col">
+                  <div class="card mt-5 ">
+                    <div class="card-header">
+                      <h2 class="display-6 text-center">Shipments Underway</h2>
+                    </div>
+                    <div class="card-body">
+                      <table class="table table-bordered text-center">
+                        <tr class="bg-dark text-white">
+                          <td>ID</td>
+                          <td>Sender</td>
+                          <td>Receiver</td>
+                          <td>Mobile</td>
+                          <td>Address</td>
+                          <td>Cargo</td>
+                          <td>Tracking No</td>
+                          <td>Status</td>
+                        </tr>
+                        <tr>
+                        <?php
+                          while($row = mysqli_fetch_assoc($shipments_result))
+                          {
+                        ?>
+                          <td><?php echo $row['ID']?></td>
+                          <td><?php echo $row['Sender']?></td>
+                          <td><?php echo $row['Receiver']?></td>
+                          <td><?php echo $row['Mobile']?></td>
+                          <td><?php echo $row['Address']?></td>
+                          <td><?php echo $row['Cargo']?></td>
+                          <td><?php echo $row['Tracking_No']?></td>
+                          <td><a href="#" class="btn btn-success">Mark Complete</a></td>
+                        </tr>
+                          <?php
+                            }
+                          ?>
+                      </table>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        </div><br><br>
+      </div>
+    </div><br>
+
+    <!-- End of page -->
+  </div>
+
+    <!-- Footer -->
+  <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-xlarge"
+          style="margin-left: 10%; background-color: rgb(214, 214, 214);">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <p class="w3-medium">Fargo Courier™️ 2024. All rights reserved.</a></p>
+  </footer>
+
 </body>
 </html>
-<?php
-   include("footer.html");
-?>
--- tracking_no.php
+++ tracking_no.php
@@ -5,26 +5,27 @@
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Cargo Confirmation</title>
+      <title>Confirmation Page</title>
    </head>
    <body style="text-align: center; padding-top: 10%; zoom: 1.5">
-      <p style="color: red; font-size: 18px">Keep this number safely you will
-         need it to track your shipment</p>
+      <p style="color: red; font-size: 18px"><em>Keep this number safely you will
+         need it to track your shipment</em></p>
       <hr> <br>
       <h2>Unique Tracking No: </h2>
    </body>
 </html>
 <?php
    $cargo = $_POST["Cargo"];
-
-   $random_text = "ABBTS";
+   $random_text = "FARGO";
    $random_no = null;
 
    for ($i = 1; $i <= 5; $i++) {
       $random_no = $random_no . random_int(0, 9);
    }
 
-   echo $random_text . "/" . $random_no . "/" . "2022_$cargo<br>";
+   $tracking_no = $random_text . "/" . $random_no . "/" . "$cargo";
+
+   echo $tracking_no;
 
    // Track shipments in DB
    if(isset($_POST["send"])) {
@@ -34,13 +35,14 @@
       $address = filter_input(INPUT_POST, "Address", FILTER_SANITIZE_SPECIAL_CHARS);
       $cargo = filter_input(INPUT_POST, "Cargo", FILTER_SANITIZE_SPECIAL_CHARS);
 
-      $sql = "INSERT INTO shipments (sender, receiver, mobile, address, cargo)
-              VALUES ('$senderName', '$receiverName', '$mobile', '$address', '$cargo')";
+      $sql = "INSERT INTO shipments (sender, receiver, mobile, address, cargo, tracking_no)
+              VALUES ('$senderName', '$receiverName', '$mobile', '$address', '$cargo', '$tracking_no')";
       
       try {
          mysqli_query($conn, $sql);
          echo '<script>alert("Order successful")</script>';
-      } catch (mysqli_sql_exception) {
+      }
+      catch (mysqli_sql_exception) {
          echo "Order unsuccessful";
       }
    }
